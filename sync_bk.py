17ef57f99faeaf425bcb6116514e56b7cfbffe80
neutrak <f@u.net>
2015-03-01 16:36:44 -0600

    added some cases for resolving conflicts, and made the extracting skip files which have no differences

--- sync_bk.py
+++ sync_bk.py
@@ -7,6 +7,7 @@
 import sys
 import os
 import shutil
+import subprocess
 
 #general libraries
 import tempfile
@@ -98,22 +99,55 @@ def mk_bk(manifest,output_file,sync_dir):
 	print('removing '+os.getcwd()+'/'+SYNC_SUBDIR+'/ '+manifest_basename)
 	os.system('rm -rf '+SYNC_SUBDIR+'/ '+manifest_basename)
 
-def resolv_bk(from_file,to_file):
-	print('Changes were found to the following file: '+to_file)
-	print('Would you like to keep the ')
-	print("\t"+'[n]ewest')
-	print("\t"+'[o]ldest')
-	print("\t"+'[l]argest')
-	print("\t"+'[s]mallest')
-	print("\t"+'[a]rchive')
-	print("\t"+'[f]ilesystem')
-	print('or would you like to ')
-	print("\t"+'view [d]iff')
-	print("\t"+'[m]erge')
-	print("?")
+def resolve_bk(src,dest):
+	got_opt=False
 	
-	option=(input().lower())[0]
-	print('Got option '+str(option))
+	while(not got_opt):
+		print('Changes were found to the following file: '+dest)
+		print('Would you like to keep the ')
+		print("\t"+'[n]ewest')
+		print("\t"+'[o]ldest')
+		print("\t"+'[l]argest')
+		print("\t"+'[s]mallest')
+		print("\t"+'[a]rchive')
+		print("\t"+'[f]ilesystem')
+		print('or would you like to ')
+		print("\t"+'view [d]iff')
+		print("\t"+'[m]erge')
+		print("?")
+		
+		option=(input().lower())[0]
+		print('Got option '+str(option))
+		
+		#TODO: do what the option asked
+		if(option=='n'):
+			#keep newest
+			got_opt=True
+		elif(option=='o'):
+			#keep oldest
+			got_opt=True
+		elif(option=='l'):
+			#keep largest
+			got_opt=True
+		elif(option=='s'):
+			#keep smallest
+			got_opt=True
+		elif(option=='a'):
+			#keep archived copy (copy source to dest)
+			got_opt=True
+		elif(option=='f'):
+			#keep filesystem copy (do nothing)
+			got_opt=True
+		elif(option=='d'):
+			#view diff
+			got_opt=True
+		elif(option=='m'):
+			#merge
+			got_opt=True
+		else:
+			print('Unrecognized option, try again')
+	
+	#issue should now be resolved
 
 #get all files (recursive os.listdir)
 def full_file_list(start_path):
@@ -134,12 +168,24 @@ def sync_bk(sync_file,sync_dir):
 	run_dir=os.getcwd()
 	
 	sync_path=sync_dir+'/'+SYNC_SUBDIR
-	os.mkdir(sync_path)
+	if(not os.path.exists(sync_path)):
+		os.mkdir(sync_path)
+	else:
+		print('Warn: '+sync_path+' already existed; if this script was previously run without a clean exit you should delete this first')
+		print('Delete? (y/n)')
+		option=(input().lower())[0]
+		print('Got option '+str(option))
+		
+		if(option=='y'):
+			print('removing and re-making '+sync_path+'...')
+			os.system('rm -rf '+sync_path)
+			os.mkdir(sync_path)
+	
 	print(os.getcwd()+'$ tar xvzf -C \''+sync_path+'\' '+sync_file)
 	os.system('tar -C \''+sync_path+'\' -xvzf '+sync_file)
 	
 	files=os.listdir(sync_path)
-	print('files (manifest+) are '+str(files))
+#	print('files (manifest+) are '+str(files))
 	
 	#after extracting, go to where the files were extracted
 	os.chdir(sync_path)
@@ -170,31 +216,45 @@ def sync_bk(sync_file,sync_dir):
 		arc_files[idx]=arc_files[idx][len(sync_path)+len('/'):]
 		src=arc_files[idx]
 		dest=start_path+arc_files[idx][len(SYNC_SUBDIR):]
-		print('got archived file at path     ./'+src)
-		print('possible destination          '+dest)
+#		print('got archived file at path     ./'+src)
+#		print('possible destination          '+dest)
 		
-		#TODO: resolve conflicts in this case
-	#	if the destination file does exist
-	#		check if there are differences using 'diff'
-	#		if so, prompt the user to resolve the differences
-	#		else just skip it (no differences)
-	
-	#	if the destination file doesn't already exist
-	#		look for a file with the same name
-	#			if a file with the same name is found
-	#				run diff
-	#				if there are no differences, prompt to move local file
-	#				if there are differences, prompt for directory and what to do
-	#		copy it to the appropriate destination
+		#if the destination file does exist
+		if(os.path.isfile(dest)):
+			#check if there are differences using 'diff'
+			command=['diff',src,dest]
+			sub_proc=subprocess.Popen(' '.join(command),stderr=subprocess.STDOUT,stdout=subprocess.PIPE,shell=True)
+			output,error=sub_proc.communicate()
+			output=output.decode('utf-8')
+			
+			#if so, prompt the user to resolve the differences
+			if(len(output)>0):
+				print('found differences between '+src+' and '+dest+'!')
+				
+				#resolve conflicts in this case
+				resolve_bk(src,dest)
+			#else just skip it (no differences)
+			else:
+				print('no differences found; skipping '+dest+'...')
+				continue
+		#if the destination file doesn't already exist
+		else:
+			print('destination '+dest+' does not yet exist! (new or moved file?)')
+			
+			#TODO: handle moved files gracefully
+			#look for a file with the same name
+				#if a file with the same name is found
+					#run diff
+					#if there are no differences, prompt to move local file
+					#if there are differences, prompt for directory and what to do
+			#TODO: copy src to the appropriate destination
 	
 	#clean up backup dir
 	os.chdir(run_dir)
 	print('removing '+sync_path)
 	os.system('rm -rf '+sync_path)
-	
-	pass
 
-#TODO: write this
+#TODO: write this, and provide a cli option to use it
 #difference a two backup files by using tar tvf and diff
 def diff_bk():
 	pass
